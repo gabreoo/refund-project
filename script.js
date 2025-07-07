5e9c49e7ca7bc7512b1fa8c0869fae6f18416ae8
Guilherme Carlos <grcarlos.dev@gmail.com>
2025-07-07 17:01:29 -0300

    input agora com valor em real

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // seleciona os elementos do formulário.
-const amount = document.getElementById('amount')
+const amount = document.getElementById("amount");
 
-
-
+// Captura o evento de input para formatar o valor.
 amount.oninput = () => {
-  let value = amount.value.replace(/\D/g, "")
-  amount.value = value
-}
+  let value = amount.value.replace(/\D/g, "");
+  
+  // Primeiro troca o valor de texto para número e depois converte para centavos para fazer a mudança de formato 
+  // (exemplo: 150 / 100 = 1.5 que é 1,50)
+  value = Number(value) / 100
 
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
